03e1bb4fc48f2b4d83cc74bea0cacf01982f1ce1
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-02-01 00:28:31 +0000

    Installer saves to autorandr

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 sudo dnf install -y xrandr
 sudo dnf install -y autorandr
 
+./force-scale.sh 1.6
+autorandr --save scaled --force
+
 sudo cp force-scale.sh /usr/local/bin/
 cp forceScale.desktop ~/.config/autostart/
